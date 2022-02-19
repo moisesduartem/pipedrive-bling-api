d0415676f606bd1ebd14104bcd345b5b4c41a6ed
Moisés Mariano <moisesduartem@gmail.com>
2022-02-19 11:09:43 -0300

    initial commit =)

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config({
+  path: `environments/.env.${process.env.NODE_ENV.trim()}`,
+});
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  return response.json({
+    message: 'Hello!'
+  });
+});
+
+const server = app.listen(process.env.APP_PORT);
+
+export { app, server };
